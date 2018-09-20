5fa1898c92d10fec6d54d56f23fb644024883a2c
fengzheng <2581379953@qq.com>
2018-09-20 12:45:04 +0800

    主类生成

---
+++ src/main/com.yp/Main.java
@@ -0,0 +1,12 @@
+package com.yp;
+
+/**
+ * @author fengzheng
+ * @create 2018-09-20 12:43
+ * @desc
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("输出测试");
+    }
+}
